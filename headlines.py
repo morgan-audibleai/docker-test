9daf1421815022217aecee036375bc9648e65c5c
Morgan Cox <mdcox@mymail.mines.edu>
2020-09-21 12:17:17 -0600

    Testing Docker

---
+++ headlines.py
@@ -0,0 +1,12 @@
+# headlines.py
+
+import parse
+from reader import feed
+
+tutorial = feed.get_article(0)
+headlines = [
+    r.named["header"]
+    for r in parse.findall("\n## {header}\n", tutorial)
+]
+print("\n".join(headlines))
+print("Okay that didnt work?")
